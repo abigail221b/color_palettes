9f357d77732c964023bda201f44336eb377a9cdf
Abigail Ponce <abigaiil221b@gmail.com>
2022-03-27 13:44:49 -0700

    Setup MySQL database connection

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ if(process.env.NODE_ENV != "production") {
     require("dotenv").config();
 }
 
+// Setup MySQL database connection
+const mysql = require("mysql2");
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+connection.connect();
+
 app.get("/", (req, res) => {
     res.send("Color Palettes");
 });
