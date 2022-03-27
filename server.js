bdc64d2c3ef3eaa279bfe34db2cc1f85153199a5
Abigail Ponce <abigaiil221b@gmail.com>
2022-03-27 13:44:02 -0700

    Load env variables

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = process.env.port || 5000;
 
+// Load db environment variables
+if(process.env.NODE_ENV != "production") {
+    require("dotenv").config();
+}
+
 app.get("/", (req, res) => {
     res.send("Color Palettes");
 });
