de48c7b22591a674e925dbf3375aafced4614a0e
Abigail Ponce <abigaiil221b@gmail.com>
2022-03-27 15:31:27 -0700

    Add middleware for access to req.body and req.params data

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
     res.send("Color Palettes");
 });
