cecbd27becc7abe1aa30b363775ac4697bbcd9ae
Abigail Ponce <abigaiil221b@gmail.com>
2022-04-02 13:59:03 -0700

    Place Create Form styles in Create.js

--- front_end/src/routes/Create.js
+++ front_end/src/routes/Create.js
@@ -23,15 +23,30 @@ function Create() {
         .then(res => console.log(res));
     }
 
+    const create_form_style = {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        marginLeft: "auto",
+        marginRight: "auto",
+        maxWidth: "500px",
+        border: "1px solid black"
+    };
+
+    const color_input_style = {
+        height: "300px",
+        border: "none"
+    };
+
     return (
-        <div className="Create">
+        <div className="Create" style={ create_form_style }>
             <h1>Create a new color palette: </h1>
             <div className="colors_input flex five">
-                <input type="color" value={ colors[0] } onChange={ (e) => handleColorChange(e, 0) }/>
-                <input type="color" value={ colors[1] } onChange={ (e) => handleColorChange(e, 1) }/>
-                <input type="color" value={ colors[2] } onChange={ (e) => handleColorChange(e, 2) }/>
-                <input type="color" value={ colors[3] } onChange={ (e) => handleColorChange(e, 3) }/>
-                <input type="color" value={ colors[4] } onChange={ (e) => handleColorChange(e, 4) }/>
+                <span><input type="color" value={ colors[0] } onChange={ (e) => handleColorChange(e, 0) } style={ color_input_style } /></span>
+                <span><input type="color" value={ colors[1] } onChange={ (e) => handleColorChange(e, 1) } style={ color_input_style } /></span>
+                <span><input type="color" value={ colors[2] } onChange={ (e) => handleColorChange(e, 2) } style={ color_input_style } /></span>
+                <span><input type="color" value={ colors[3] } onChange={ (e) => handleColorChange(e, 3) } style={ color_input_style } /></span>
+                <span><input type="color" value={ colors[4] } onChange={ (e) => handleColorChange(e, 4) } style={ color_input_style } /></span>
             </div>
             <button onClick={ handleSubmit }>Create</button>
         </div>
