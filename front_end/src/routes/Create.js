5c3756665052bce31a7118f3d9a693fef96f4b93
Abigail Ponce <abigaiil221b@gmail.com>
2022-04-01 18:06:16 -0700

    Implement create palette form

--- front_end/src/routes/Create.js
+++ front_end/src/routes/Create.js
@@ -1,5 +1,29 @@
+import { useState } from "react";
+
 function Create() {
-    return "Create Palette";
+    const [colors, setColors] = useState(["#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000"]);
+
+    function handleColorChange(e, id) {
+        let newColors = colors.map( (color, index) => {
+            if(id === index) return e.target.value;
+            return color;
+        });
+        setColors(newColors);
+    }
+
+    return (
+        <div className="Create">
+            <h1>Create a new color palette: </h1>
+            <div className="colors_input flex five">
+                <input type="color" value={ colors[0] } onChange={ (e) => handleColorChange(e, 0) }/>
+                <input type="color" value={ colors[1] } onChange={ (e) => handleColorChange(e, 1) }/>
+                <input type="color" value={ colors[2] } onChange={ (e) => handleColorChange(e, 2) }/>
+                <input type="color" value={ colors[3] } onChange={ (e) => handleColorChange(e, 3) }/>
+                <input type="color" value={ colors[4] } onChange={ (e) => handleColorChange(e, 4) }/>
+            </div>
+            <button onClick={ handleSubmit }>Create</button>
+        </div>
+    );
 }
 
 export default Create;
