4fc4f07ca33dd9b73e118eb46df1ccc5b27dc14e
Abigail Ponce <abigaiil221b@gmail.com>
2022-04-01 21:35:33 -0700

    Handle fetch and rendering of popular palettes according time filter

--- front_end/src/routes/Popular.js
+++ front_end/src/routes/Popular.js
@@ -1,5 +1,64 @@
+import { useEffect, useState } from "react";
+
+const TIME_FILTER = {
+    ALL_TIME: "all_time",
+    THIS_YEAR: "this_year",
+    THIS_MONTH: "this_month",
+    THIS_WEEK: "this_week"
+};
+
 function Popular() {
-    return "Popular Palettes";
+
+    const [palettes, setPalettes] = useState([]);
+    const [filter, setFilter] = useState(TIME_FILTER.ALL_TIME);
+    const [page, setPage] = useState(0);
+
+    function handleChange(e) {
+        switch(e.target.value) {
+            case TIME_FILTER.THIS_YEAR:
+                setFilter(TIME_FILTER.THIS_YEAR);
+                break;
+            case TIME_FILTER.THIS_MONTH:
+                setFilter(TIME_FILTER.THIS_MONTH);
+                break;
+            case TIME_FILTER.THIS_WEEK:
+                setFilter(TIME_FILTER.THIS_WEEK);
+                break;
+            default:
+                setFilter(TIME_FILTER.ALL_TIME);
+        }
+    }
+
+    useEffect(() => {
+        fetch(`/palettes/popular/${filter}/${ page }`)
+        .then(res => res.json())
+        .then(data => setPalettes(data));
+    }, [filter, page]);
+
+    return (
+        <div className="Popular">
+            <h1>Popular Palettes</h1>
+            <select name="filer" onChange={ handleChange }>
+                <option value={ TIME_FILTER.ALL_TIME }>All Time</option>
+                <option value={ TIME_FILTER.THIS_YEAR }>This Year</option>
+                <option value={ TIME_FILTER.THIS_MONTH }>This Month</option>
+                <option value={ TIME_FILTER.THIS_WEEK }>This Week</option>
+            </select>
+
+            {palettes.map(palette =>
+                <div className="ColorPalette">
+                    <span>{ palette.color0 }</span>
+                    <span>{ palette.color1 }</span>
+                    <span>{ palette.color2 }</span>
+                    <span>{ palette.color3 }</span>
+                    <span>{ palette.color4 }</span>
+                    <span>{ palette.date_created }</span>
+                    <span>{ palette.num_likes }</span>
+                </div>)}
+
+        </div>
+    );
+
 }
 
 export default Popular;
