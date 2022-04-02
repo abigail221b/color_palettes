2f2708ff9507503930e68f856c2df367fb2ff14e
Abigail Ponce <abigaiil221b@gmail.com>
2022-04-01 18:11:49 -0700

    Style App navigation and layout

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -5,13 +5,24 @@ function App() {
     return (
         <div className="App">
             <nav>
-                <Link to="popular">Popular</Link>
-                <Link to="new">New</Link>
-                <Link to="random">Random</Link>
-                <Link to="liked_palettes">Liked Palettes</Link>
-                <Link to="create">Create</Link>
+                <Link to="popular">
+                    <span className="brand">Color Palettes</span>
+                </Link>
+
+                <input id="bmenub" type="checkbox" className="show" />
+                <label for="bmenub" className="burger pseudo button">Menu</label>
+
+                <div className="menu">
+                    <Link to="popular">Popular</Link>
+                    <Link to="new">New</Link>
+                    <Link to="random">Random</Link>
+                    <Link to="liked_palettes">Liked Palettes</Link>
+                    <Link to="create"><button>Create</button></Link>
+                </div>
             </nav>
-            <Outlet />
+            <div className="wrapper">
+                <Outlet />
+            </div>
         </div>
     );
 }
