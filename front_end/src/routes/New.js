b7043413e86ddff4ba97eb2227bf9cdc35d43490
Abigail Ponce <abigaiil221b@gmail.com>
2022-04-22 11:10:43 -0700

    Render New ColorPalettes responsively

--- front_end/src/routes/New.js
+++ front_end/src/routes/New.js
@@ -18,10 +18,12 @@ function New() {
     return (
         <div className="New" style={{ maxWidth: "1000px", marginLeft: "auto", marginRight: "auto" }}>
             <h1>New Color Palettes</h1>
-            {palettes.map(palette => <ColorPalette
-                                        colors={[palette.color0, palette.color1, palette.color2, palette.color3, palette.color4]}
-                                        date_created = { palette.date_created}
-                                        num_likes = { palette.num_likes }/> )}
+            <div className="flex one two-500 three-800">
+                {palettes.map(palette => <ColorPalette
+                                            colors={[palette.color0, palette.color1, palette.color2, palette.color3, palette.color4]}
+                                            date_created = { palette.date_created}
+                                            num_likes = { palette.num_likes }/> )}
+            </div>
         </div>
     );
 }
