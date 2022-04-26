82aa99a499d92d692e2f4ce14068de87d7b615a7
Abigail Ponce <abigaiil221b@gmail.com>
2022-04-26 13:29:23 -0700

    Fix responsive style for New Palettes

--- front_end/src/routes/New.js
+++ front_end/src/routes/New.js
@@ -16,10 +16,10 @@ function New() {
     }, [page])
 
     return (
-        <div className="New" style={{ maxWidth: "1000px", marginLeft: "auto", marginRight: "auto" }}>
+        <div className="New" style={{ maxWidth: "1200px", marginLeft: "auto", marginRight: "auto" }}>
             <h1>New Color Palettes</h1>
-            <div className="flex one two-500 three-800">
-                {palettes.map(palette => <div><ColorPalette
+            <div className="flex one two-500 three-700 four-1000">
+                {palettes.map(palette => <div style={{ padding:"10px" }}><ColorPalette
                                             colors={[palette.color0, palette.color1, palette.color2, palette.color3, palette.color4]}
                                             date_created = { palette.date_created}
                                             num_likes = { palette.num_likes }/></div> )}
