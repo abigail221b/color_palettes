258687910d802c0c5b82db7e5e3fea3b18316a17
Abigail Ponce <abigaiil221b@gmail.com>
2022-04-26 16:36:25 -0700

    Initialize Random page

--- front_end/src/routes/Random.js
+++ front_end/src/routes/Random.js
@@ -1,5 +1,22 @@
+import { useEffect, useState } from "react";
+import ColorPalette from "../components/ColorPalette.js";
+
 function Random() {
-    return "Random Palettes";
+
+    const [palettes, setPalettes] = useState([]);
+    const [page, setPage] = useState(0);
+
+    return (
+        <div className="Random" style={{ maxWidth: "1200px", marginLeft: "auto", marginRight: "auto" }}>
+            <h1>Random Color Palettes</h1>
+            <div className="flex one two-500 three-700 four-1000">
+                {palettes.map(palette => <div style={{ padding:"10px" }}><ColorPalette
+                                            colors={[palette.color0, palette.color1, palette.color2, palette.color3, palette.color4]}
+                                            date_created = { palette.date_created}
+                                            num_likes = { palette.num_likes }/></div> )}
+            </div>
+        </div>
+    );
 }
 
 export default Random;
