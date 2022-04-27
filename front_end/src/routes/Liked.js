ee59f914c40a9f9e27f713898817bca32eab354e
Abigail Ponce <abigaiil221b@gmail.com>
2022-04-27 00:29:41 -0700

    Initalize Liked component

--- front_end/src/routes/Liked.js
+++ front_end/src/routes/Liked.js
@@ -1,5 +1,9 @@
 function Liked() {
-    return "Liked Palettes";
+    return (
+        <div className="Liked" style={{  maxWidth: "1200px", marginLeft: "auto", marginRight: "auto" }}>
+            <h1>Liked Color Palettes</h1>
+        </div>
+    );
 }
 
 export default Liked;
