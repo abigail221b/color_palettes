e6d15edea15bd34fffe795e54e5aca07cd713bcb
Abigail Ponce <abigaiil221b@gmail.com>
2022-04-27 00:32:38 -0700

    Set initial state to liked palettes in localStorage

--- front_end/src/routes/Liked.js
+++ front_end/src/routes/Liked.js
@@ -1,4 +1,15 @@
+import { useEffect, useState } from "react";
+
 function Liked() {
+    const [palettes, setPalettes] = useState(() => {
+        let saved = localStorage.getItem("palettes");
+        if(saved) {
+            return JSON.parse(saved);
+        }
+        return [];
+    });
+    const [page, setPage] = useState(0);
+
     return (
         <div className="Liked" style={{  maxWidth: "1200px", marginLeft: "auto", marginRight: "auto" }}>
             <h1>Liked Color Palettes</h1>
