2cdaca307f519ed2d154763c30b25f92c45f7327
Abigail Ponce <abigaiil221b@gmail.com>
2022-05-04 12:48:45 -0700

    Add key prop to ColorPalette

--- front_end/src/pages/New.js
+++ front_end/src/pages/New.js
@@ -19,10 +19,12 @@ function New() {
         <div className="New" style={{ maxWidth: "1200px", marginLeft: "auto", marginRight: "auto" }}>
             <h1>New Color Palettes</h1>
             <div className="flex one two-500 three-700 four-1000">
-                {palettes.map(palette => <div style={{ padding:"10px" }}><ColorPalette
-                                            colors={[palette.color0, palette.color1, palette.color2, palette.color3, palette.color4]}
-                                            date_created = { palette.date_created}
-                                            num_likes = { palette.num_likes }/></div> )}
+                {palettes.map(palette => <div style={{ padding:"10px" }}>
+                                            <ColorPalette
+                                                key={ palette.color0 + palette.color1 + palette.color2 + palette.color3 + palette.color4 }
+                                                colors={[palette.color0, palette.color1, palette.color2, palette.color3, palette.color4]}
+                                                date_created = { palette.date_created}
+                                                num_likes = { palette.num_likes }/></div> )}
             </div>
         </div>
     );
--- front_end/src/pages/Popular.js
+++ front_end/src/pages/Popular.js
@@ -46,10 +46,12 @@ function Popular() {
                 <option value={ TIME_FILTER.THIS_WEEK }>This Week</option>
             </select>
             <div className="flex one two-500 three-700 four-1000">
-                {palettes.map(palette => <div style={{ padding:"10px" }}><ColorPalette
-                                            colors={[palette.color0, palette.color1, palette.color2, palette.color3, palette.color4]}
-                                            date_created = { palette.date_created}
-                                            num_likes = { palette.num_likes }/></div> )}
+                {palettes.map(palette => <div style={{ padding:"10px" }}>
+                                            <ColorPalette
+                                                key={ palette.color0 + palette.color1 + palette.color2 + palette.color3 + palette.color4 }
+                                                colors={[palette.color0, palette.color1, palette.color2, palette.color3, palette.color4]}
+                                                date_created = { palette.date_created}
+                                                num_likes = { palette.num_likes }/></div> )}
             </div>
         </div>
     );
