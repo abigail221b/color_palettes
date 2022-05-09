e84d48db3384b716fe1f08cdc244ea4c103d4d9b
Abigail Ponce <abigaiil221b@gmail.com>
2022-05-09 16:26:45 -0700

    Change process.env.port to process.env.PORT

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 // Load db environment variables
 if(process.env.NODE_ENV != "production") {
