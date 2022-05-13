5348eb0bca294f5ecddec34bf9f764080abe2972
Abigail Ponce <abigaiil221b@gmail.com>
2022-05-13 14:31:47 -0700

    Remove database connection set up

--- server.js
+++ server.js
@@ -2,22 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Load db environment variables
-if(process.env.NODE_ENV != "production") {
-    require("dotenv").config();
-}
-
-// Setup MySQL database connection
-const mysql = require("mysql2");
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    multipleStatements: true
-});
-connection.connect();
-
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
