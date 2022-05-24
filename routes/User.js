162453dd53986181575881df3e966567fdc7821d
Abigail Ponce <abigaiil221b@gmail.com>
2022-05-24 15:30:22 -0700

    Fix query to fetch user's liked palettes

--- routes/User.js
+++ routes/User.js
@@ -4,7 +4,7 @@ const pool = require("../database_pool.js");
 
 router.get("/likes", (req, res) => {
     pool.query(`SELECT *
-                FROM user_likes_palette
+                FROM user_likes_palette ulp LEFT JOIN color_palette p ON ulp.color0=p.color0 AND ulp.color1=p.color1 AND ulp.color2=p.color2 AND ulp.color3=p.color3 AND ulp.color4=p.color4
                 WHERE username='${ req.query.username }'`,
         (err, rows) => {
             if (err) throw err;
