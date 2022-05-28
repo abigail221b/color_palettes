77fbcab91c556062f4eaac9d30b4a707fa9e26b5
Abigail Ponce <abigaiil221b@gmail.com>
2022-05-28 15:33:56 -0700

    Fetch user likes from db on App start up, then store in localStorage

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,7 +1,18 @@
 import { Link, Outlet } from "react-router-dom";
+import { useEffect } from "react";
 import './App.css';
 
 function App() {
+
+    fetch("/user/demo_user/palettes/likes")
+    .then(res => res.json())
+    .then(palettes => {
+        let store = palettes.map(palette => {
+            return palette.color0 + palette.color1 + palette.color2 + palette.color3 + palette.color4;
+        });
+        localStorage.setItem("palettes", JSON.stringify(store));
+    });
+
     return (
         <div className="App">
             <nav>
