91d8cb34e38793e1c463c3839e2084f4af872a65
Abigail Ponce <abigaiil221b@gmail.com>
2022-06-15 16:10:52 -0700

    Add route to get a user's collection of color palettes

--- routes/User.js
+++ routes/User.js
@@ -2,6 +2,16 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../database_pool.js");
 
+router.get("/:username/palettes", (req, res) => {
+    pool.query(`SELECT *
+                FROM user_creates_palette ucp LEFT JOIN color_palette p ON ucp.color0=p.color0 AND ucp.color1=p.color1 AND ucp.color2=p.color2 AND ucp.color3=p.color3 AND ucp.color4=p.color4
+                WHERE username='${ req.params.username }'`,
+        (err, rows) => {
+            if (err) throw err;
+            res.send(rows);
+        });
+});
+
 router.get("/:username/palettes/likes", (req, res) => {
     pool.query(`SELECT *
                 FROM user_likes_palette ulp LEFT JOIN color_palette p ON ulp.color0=p.color0 AND ulp.color1=p.color1 AND ulp.color2=p.color2 AND ulp.color3=p.color3 AND ulp.color4=p.color4
