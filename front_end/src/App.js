ff558f97088dd4667dfbf208696934cfcf65ed18
Abigail Ponce <abigaiil221b@gmail.com>
2022-06-15 16:01:34 -0700

    Add a responsive menu for user links and account info

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
 
     const [isMenuExpanded, setIsMenuExpanded] = useState(false);
+    const [isSmallMenuExpanded, setIsSmallMenuExpanded] = useState(false);
 
     fetch("/user/demo_user/palettes/likes")
     .then(res => res.json())
@@ -24,20 +25,34 @@ function App() {
                     <span className="brand">Color Palettes</span>
                 </Link>
 
-                <input id="bmenub" type="checkbox" className="show" />
+                <input id="bmenub" type="checkbox" className="show" onChange={() => setIsMenuExpanded(isMenuExpanded => !isMenuExpanded)}/>
                 <label for="bmenub" className="burger pseudo button">Menu</label>
 
                 <div className="menu">
                     <Link to="popular">Popular</Link>
                     <Link to="new">New</Link>
-                    <a style={{"position":"relative", "cursor":"pointer"}} onClick={() => setIsMenuExpanded(isMenuExpanded => !isMenuExpanded)}>
-                        demo_user
-                        <div style={isMenuExpanded? {"border":"1px solid black", "padding":"10px" ,"display":"flex", "flex-direction":"column", "position": "absolute", "minWidth":"200px", "textAlign": "left"} : {"display":"none"}}>
+                    {isMenuExpanded?
+                        <>
                             <Link to="create">Create</Link>
                             <Link to="likes">Likes</Link>
                             <Link to="collection">Collection</Link>
-                        </div>
-                    </a>
+                        </> :
+                        <>
+                            <a style={{"position":"relative", "cursor":"pointer"}} onClick={() => setIsSmallMenuExpanded(isSmallMenuExpanded => !isSmallMenuExpanded)}>
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="25" viewBox="0 0 19 20" width="25"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/></svg>
+                                    <div className="card" style={isSmallMenuExpanded? {"padding":"10px" ,"display":"flex", "flex-direction":"column", "position": "absolute", "right":"0px", "width":"250px", "textAlign": "left"} : {"display":"none"}}>
+                                        <header>Hello demo_user!</header>
+                                        <footer>
+                                            <Link to="create"><span className="stack pseudo button" style={{"width":"100%"}}>Create</span></Link>
+                                            <Link to="likes"><span className="stack pseudo button" style={{"width":"100%"}}>Likes</span></Link>
+                                            <Link to="collection"><span className="stack pseudo button" style={{"width":"100%"}}>Collection</span></Link>
+                                        </footer>
+                                    </div>
+                                </span>
+                            </a>
+                        </>
+                    }
                 </div>
             </nav>
             <div className="wrapper">
