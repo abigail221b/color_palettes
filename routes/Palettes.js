70510389c551492645ce18400abd2e23c86b8294
Abigail Ponce <abigaiil221b@gmail.com>
2022-06-18 14:42:05 -0700

    Handle request for a specific color palette

--- routes/Palettes.js
+++ routes/Palettes.js
@@ -85,4 +85,19 @@ router.post("/", (req, res) => {
     });
 });
 
+// Get color palette by its colors
+router.get("/:color0/:color1/:color2/:color3/:color4", (req, res) => {
+    pool.query(`SELECT *
+                FROM color_palette
+                WHERE color0 = '${ req.params.color0 }'
+                    AND color1 = '${ req.params.color1 }'
+                    AND color2 = '${ req.params.color2 }'
+                    AND color3 = '${ req.params.color3 }'
+                    AND color4 = '${ req.params.color4 }'`,
+        (err, rows) => {
+            if (err) throw err;
+            res.send(rows[0]);
+        });
+});
+
 module.exports = router;
