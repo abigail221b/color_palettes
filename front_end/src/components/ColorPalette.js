c95113a7b2a8acb531a8d9c736bf36873dc74565
Abigail Ponce <abigaiil221b@gmail.com>
2022-06-17 21:30:02 -0700

    Style ColorPalette card footer with creator username

--- front_end/src/components/ColorPalette.js
+++ front_end/src/components/ColorPalette.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-function ColorPalette({ colors, date_created, num_likes}) {
+function ColorPalette({ creator, colors, date_created, num_likes}) {
 
     const [liked, setLiked] = useState(() => {
         let saved = localStorage.getItem("palettes");
@@ -15,7 +15,7 @@ function ColorPalette({ colors, date_created, num_likes}) {
     const [numLikes, setNumLikes] = useState(num_likes);
 
     const colorPalette_style = {
-        height: "225px",
+        height: "235px",
         width: "100%",
         padding: 0,
         display: "flex",
@@ -37,11 +37,11 @@ function ColorPalette({ colors, date_created, num_likes}) {
 
     const paletteInfo_style = {
         width: "100%",
-        height: "70px",
+        height: "75px",
         display: "flex",
         justifyContent: "space-between",
         alignItems: "center",
-        padding: "0 10px"
+        padding: "0 15px"
     };
 
     const formatDate = (date) => {
@@ -105,8 +105,14 @@ function ColorPalette({ colors, date_created, num_likes}) {
                 { colors.map(color => <div style={{ ...colorBlock_style, backgroundColor:`#${color}` }}></div>) }
             </div>
             <div className="info" style={ paletteInfo_style }>
-                <div>{ formatDate(date_created) }</div>
-                <button style={{ backgroundColor: liked? "pink" : "white", border: "2px solid pink" }} onClick={ handleLike }>&#9829; { numLikes }</button>
+                <div>
+                    <div style={{ "fontSize": "medium"}}>{ creator }</div>
+                    <div style={{ "fontSize": "small"}}>{ formatDate(date_created) }</div>
+                </div>
+                <button style={{ backgroundColor: liked? "pink" : "white", border: "2px solid pink" }} onClick={ handleLike }>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -5 26 26" width="20"><path d="M0 0h24v24H0z" fill="none"/><path stroke-width=".15" stroke="white" d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z"/></svg>
+                    { numLikes }
+                </button>
             </div>
         </div>
     );
