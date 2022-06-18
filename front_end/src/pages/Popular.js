707be11b47b1c16a2b775e08dbc05636074d8483
Abigail Ponce <abigaiil221b@gmail.com>
2022-06-18 15:05:55 -0700

    Change fetch url to get popular palettes

--- front_end/src/pages/Popular.js
+++ front_end/src/pages/Popular.js
@@ -31,7 +31,7 @@ function Popular() {
     }
 
     useEffect(() => {
-        fetch(`/palettes/popular/${filter}/?limit=15&page=${ page }`)
+        fetch(`/palettes/popular/?filter=${filter}&limit=15&page=${ page }`)
         .then(res => res.json())
         .then(data => setPalettes(data));
     }, [filter, page]);
