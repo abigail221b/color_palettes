5bb8dad169475f01620fbf0efc330af843a3467e
Abigail Ponce <abigaiil221b@gmail.com>
2022-06-19 14:44:50 -0700

    Add likesSlice to store

--- front_end/src/redux/store.js
+++ front_end/src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import likesReducer from "./likesSlice.js";
 
-export const store = configureStore(
-    reducer: {}
-);
+export const store = configureStore({
+    reducer: {
+        likes: likesReducer
+    }
+});
