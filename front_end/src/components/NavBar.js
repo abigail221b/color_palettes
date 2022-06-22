7b55ef2cb0e3b1fa36380a6744ba5e8cf4619d97
Abigail Ponce <abigaiil221b@gmail.com>
2022-06-21 17:40:38 -0700

    Render NavBar based on whether a user is logged in or not If user is logged in, NavBar will display user related links. If user is not logged in, NavBar will display Login and Sign up

--- front_end/src/components/NavBar.js
+++ front_end/src/components/NavBar.js
@@ -1,10 +1,60 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 function NavBar() {
 
     const [isMenuExpanded, setIsMenuExpanded] = useState(false);
     const [isSmallMenuExpanded, setIsSmallMenuExpanded] = useState(false);
+    const isLoggedIn = useSelector(state => state.loggedIn.value);
+
+    const subMenu_style = {
+        "position" : "absolute",
+        "right" : "0",
+        "minWidth": "200px",
+        "padding": "10px"
+    };
+
+    const subMenu = (
+        <div className="card" style={subMenu_style}>
+            <header>Hello!</header>
+            <footer style={{"display":"flex", "flexDirection":"column","gap":"15px"}}>
+                <Link to="create">Create</Link>
+                <Link to="likes">Likes</Link>
+                <Link to="collection">Collection</Link>
+            </footer>
+        </div>
+    );
+
+    const loggedIn_menu = (
+        <>
+            {isMenuExpanded?
+                <>
+                    <Link to="popular">Popular</Link>
+                    <Link to="new">New</Link>
+                    <Link to="create">Create</Link>
+                    <Link to="likes">Likes</Link>
+                    <Link to="collection">Collection</Link>
+                </> :
+                <>
+                    <Link to="popular">Popular</Link>
+                    <Link to="new">New</Link>
+                    <button style={{"backgroundColor":"yellow"}} onClick={() => setIsSmallMenuExpanded(isSmallMenuExpanded => !isSmallMenuExpanded)}>
+                        submenu
+                    </button>
+                    {isSmallMenuExpanded? subMenu : ""}
+                </>
+            }
+        </>
+    );
+
+    const loggedOut_menu = (
+        <>
+            <span class="button" style={{ "backgroundColor": "yellow" }}><Link to="login">Log in</Link></span>
+            <span class="button" style={{ "backgroundColor": "yellow" }}><Link to="register">Sign up</Link></span>
+        </>
+    );
+
 
     return (
         <nav>
@@ -16,11 +66,8 @@ function NavBar() {
             <label for="bmenub" className="burger pseudo button">Menu</label>
 
             <div className="menu">
-                <Link to="popular">Popular</Link>
-                <Link to="new">New</Link>
-                <Link to="create">Create</Link>
-                <Link to="likes">Likes</Link>
-                <Link to="collection">Collection</Link>
+
+                { isLoggedIn? loggedIn_menu : loggedOut_menu}
             </div>
         </nav>
     );
