006b488e5546901838f91737a4a1df14ac6a8087
Abigail Ponce <abigaiil221b@gmail.com>
2022-06-22 14:37:07 -0700

    Check if user is logged in before handling like

--- front_end/src/components/ColorPalette.js
+++ front_end/src/components/ColorPalette.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { like, unlike } from "../redux/likesSlice.js";
 
 function ColorPalette({ creator, colors, date_created, num_likes, isLikedByUser }) {
@@ -7,6 +8,7 @@ function ColorPalette({ creator, colors, date_created, num_likes, isLikedByUser 
     const [liked, setLiked] = useState(isLikedByUser);
     const [numLikes, setNumLikes] = useState(num_likes);
     const dispatch = useDispatch();
+    const isLoggedIn = useSelector(state => state.isLoggedIn.value);
 
     const colorPalette_style = {
         height: "235px",
@@ -69,20 +71,22 @@ function ColorPalette({ creator, colors, date_created, num_likes, isLikedByUser 
     }
 
     const handleLike = () => {
-        if(liked) {
-            setNumLikes(numLikes => numLikes-1);
-            fetch(`/palettes/${colors[0]}/${colors[1]}/${colors[2]}/${colors[3]}/${colors[4]}/?username=demoUser121&like=false`, {
-                method: "PUT"
-            });
-            dispatch(unlike({ color0: colors[0], color1: colors[1], color2: colors[2], color3: colors[3], color4: colors[4] }));
-        } else {
-            setNumLikes(numLikes => numLikes+1);
-            fetch(`/palettes/${colors[0]}/${colors[1]}/${colors[2]}/${colors[3]}/${colors[4]}/?username=demoUser121&like=true`, {
-                method: "PUT"
-            });
-            dispatch(like({ color0: colors[0], color1: colors[1], color2: colors[2], color3: colors[3], color4: colors[4] }));
+        if(isLoggedIn) {
+            if(liked) {
+                setNumLikes(numLikes => numLikes-1);
+                fetch(`/palettes/${colors[0]}/${colors[1]}/${colors[2]}/${colors[3]}/${colors[4]}/?username=demoUser121&like=false`, {
+                    method: "PUT"
+                });
+                dispatch(unlike({ color0: colors[0], color1: colors[1], color2: colors[2], color3: colors[3], color4: colors[4] }));
+            } else {
+                setNumLikes(numLikes => numLikes+1);
+                fetch(`/palettes/${colors[0]}/${colors[1]}/${colors[2]}/${colors[3]}/${colors[4]}/?username=demoUser121&like=true`, {
+                    method: "PUT"
+                });
+                dispatch(like({ color0: colors[0], color1: colors[1], color2: colors[2], color3: colors[3], color4: colors[4] }));
+            }
+            setLiked(!liked);
         }
-        setLiked(!liked);
     }
 
     return (
