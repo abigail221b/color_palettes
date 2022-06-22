95baeec54c8c10dad405587779e2dd8b2e396e15
Abigail Ponce <abigaiil221b@gmail.com>
2022-06-21 17:36:38 -0700

    Revert "Add isLoggedIn state to App"
    This reverts commit cdfe81218543ab727b2861eb5ddb248357b9f094.

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,13 +1,11 @@
 import NavBar from "./components/NavBar";
 import { useEffect } from "react";
 import { initalizePalettes } from "./redux/likesSlice.js";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Outlet } from "react-router-dom";
 import './App.css';
 
 function App() {
-
-    const isLoggedIn = useSelector(state => state.loggedIn.value);
 
     // Fetch user likes from database save to redux store
     const dispatch = useDispatch();
