5118593fdcab14ac00def47fe1488432f408dd58
Abigail Ponce <abigaiil221b@gmail.com>
2022-06-21 17:34:41 -0700

    Revert "Add isLoggedin prop to NavBar"
    This reverts commit 8cc631f9a3f07d36b0592d7a89d5f6a1e576edc0.
    I forgot that I can just get login state from Redux store.

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -22,7 +22,7 @@ function App() {
     return (
         <div className="App">
             <div className="wrapper">
-                <NavBar isLoggedIn={ isLoggedIn }/>
+                <NavBar />
                 <Outlet />
             </div>
         </div>
