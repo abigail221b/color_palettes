cc85b5494ed5e6666637b820e37f49b6a45dbcbe
Abigail Ponce <abigaiil221b@gmail.com>
2022-06-22 15:53:11 -0700

    Add an endpoint to handle user sign up

--- routes/User.js
+++ routes/User.js
@@ -1,5 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../database_pool.js");
+const bcrypt = require("bcrypt");
+
+router.post("/signup", (req, res) => {
+    // Determine if username exists in database
+    pool.query("SELECT * FROM user WHERE username=?", [req.body.username], (err, result) => {
+        if(result.length > 0) {
+            res.send({ msg: "Error. Username is taken."});
+        }
+        else {
+            // salt + hash password
+            bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+                // store username and password into database
+                pool.query("INSERT INTO user VALUES (?, ?)", [req.body.username, hashedPassword], (err, result) => {
+                    if(err) {
+                        res.send({ msg: "Not successful"});
+                    } else {
+                        res.send({ msg: "Successfully registered"});
+                    }
+                });
+            });
+        }
+    });
+});
 
 module.exports = router;
