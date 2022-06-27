f0093d81f5374efe686d27feea22428d101183b6
Abigail Ponce <abigaiil221b@gmail.com>
2022-06-27 13:54:25 -0700

    Add cors and cookie-parser middleware

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const palettesRouter = require("./routes/Palettes.js");
 const userRouter = require("./routes/User.js");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParser());
 
 app.use("/palettes", palettesRouter);
 app.use("/user",     userRouter);
