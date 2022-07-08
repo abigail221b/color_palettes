1072644de5db2eebea24b0893823bf0887663a1f
Abigail Ponce <abigaiil221b@gmail.com>
2022-07-08 10:12:44 -0700

    Change submenu icon

--- front_end/src/components/NavBar/NavBar.js
+++ front_end/src/components/NavBar/NavBar.js
@@ -32,9 +32,9 @@ function NavBar() {
                 <>
                     <Link to="popular">Popular</Link>
                     <Link to="new">New</Link>
-                    <button style={{"backgroundColor":"yellow"}} onClick={() => setIsSmallMenuExpanded(isSmallMenuExpanded => !isSmallMenuExpanded)}>
-                        submenu
-                    </button>
+                    <span style={{ cursor: "pointer"}} onClick={() => setIsSmallMenuExpanded(isSmallMenuExpanded => !isSmallMenuExpanded)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="-4 -4 25 25" width="25"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/></svg>
+                    </span>
                     {isSmallMenuExpanded? <SubMenu /> : ""}
                 </>
             }
