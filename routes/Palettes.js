84a34b6c014ed26efeba8232c62452fefc6d9c7f
Abigail Ponce <abigaiil221b@gmail.com>
2022-07-08 10:07:29 -0700

    Add route to delete a color palette

--- routes/Palettes.js
+++ routes/Palettes.js
@@ -168,4 +168,18 @@ router.get("/user/:username", (req, res) => {
     });
 });
 
+// Delete a color palette
+router.delete("/:color0/:color1/:color2/:color3/:color4", (req, res) => {
+    pool.query(`DELETE FROM color_palette
+                WHERE color0 = '${ req.params.color0 }'
+                    AND color1 = '${ req.params.color1 }'
+                    AND color2 = '${ req.params.color2 }'
+                    AND color3 = '${ req.params.color3 }'
+                    AND color4 = '${ req.params.color4 }'`,
+        (err, rows) => {
+            if (err) throw err;
+            res.send(rows[0]);
+        });
+});
+
 module.exports = router;
